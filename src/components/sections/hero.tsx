189095e9cfff76ddcab853a254842ead94f72308
Cyber-Q01 <147355779+Cyber-Q01@users.noreply.github.com>
2025-07-12 23:19:20 +0100

    Update hero.tsx

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -12,13 +12,11 @@ const HeroSection = () => {
       <div className="container mx-auto px-4 sm:px-6 lg:px-8 text-center">
       
     <div className=" flex justify-center items-center gap-[2px]">
-  <Image
-      src={NexogenLogo}
-      alt="NEXOGEN"
-      fill
-      className="object-contain"
-      sizes="1em"
-    />
+ <Image
+  src={NexogenLogo}
+  alt="NEXOGEN"
+  style={{ height: '1em', width: 'auto' }}
+/>
   <h1 className="text-4xl font-extrabold tracking-tighter sm:text-6xl md:text-7xl lg:text-8xl font-headline bg-clip-text text-transparent bg-gradient-to-b from-white to-gray-400">
     NEXOGEN LABS
   </h1>
