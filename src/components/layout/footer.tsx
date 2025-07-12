a672b6760c6322a7b1f575fc55a58ceb9dd121ea
Cyber-Q01 <147355779+Cyber-Q01@users.noreply.github.com>
2025-07-12 20:42:55 +0100

    Update footer.tsx

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -15,7 +15,7 @@ const Footer = () => {
 {/*             <Logo className="h-8 w-8 text-primary" /> */}
             <span className="text-lg font-bold">NEXOGEN LABS</span>
           </div>
-          <div className="flex w-[100px] justify-center border mx-auto items-center space-x-6  relative border-4 border-gray-400 p-8 pt-6">
+          <div className="flex w-[600px] justify-center border mx-auto items-center space-x-6  relative border-4 border-gray-400 p-8 pt-6">
 
 {/*             <div class="relative border-4 border-gray-400 p-8 pt-6"> */}
     <span class="absolute -top-3 left-4  px-1 text-lg font-semibold text-gray-700">
